c05d5dd6b4232c9b90377fdae73ff1f5d91b4d1a
Karishma290395 <91252844+Karishma290395@users.noreply.github.com>
2021-11-19 11:45:14 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,43 @@
+package test5;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {  
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the String ");
+		String Name = "";
+		Palindrome obj = new Palindrome();
+		Name = obj.inputString(sc);
+		String rev = "";
+		for (int i = Name.length() - 1; i >= 0; i--) {
+
+			rev = rev + Name.charAt(i);
+
+		}
+		if (rev.equals(Name)) {
+			System.out.println("The string is Palindrome");
+		} else {
+			System.out.println("The string is not Palindrome");
+
+		}
+	}
+
+	public String inputString(Scanner sc) {
+		String countName = "";
+		String Name = sc.nextLine();
+		for (int i = 0; i < Name.length(); i++) {
+			if (Name.charAt(i) >= 'a' && Name.charAt(i) <= 'z') {
+				countName = countName + Name.charAt(i);
+			} else {
+				System.out.println("Please enter small case letters ONLY.");
+				Name = sc.nextLine();
+				i--;
+			}
+		}
+		return countName;
+	}
+
+}
